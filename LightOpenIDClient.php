04a6c70b275881624c9d5e9f7c974ce9f82bc384
Christophe VG <xtof@pumpkin.local>
2010-10-05 17:51:06 +0200

    initial commit
    added LightOpenID submodule
    added LightOpenIDClient client Class
    added demo page
    added README

---
+++ LightOpenIDClient.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * LightOpenIDClient
+ * 
+ * A small Client wrapper around the very nice LightOpenID
+ * See: http://gitorious.org/lightopenid
+ * 
+ * @author Christophe VG
+ */
+
+include_once 'lightopenid/openid.php';
+
+class LightOpenIDClient {
+  private $openid;
+  private static $instance;
+  
+  public static function getInstance() {
+    if( ! self::$instance ) {
+      self::$instance = new LightOpenIDClient();
+    }
+    return self::$instance;
+  }
+
+  private function __construct() {
+    $this->openid = new LightOpenID();
+  }
+
+  private function handleLoginRequest() {
+    global $_POST;
+    if( isset($_POST['openid_identifier']) ) {
+      try {
+        $this->openid->identity = $_POST['openid_identifier'];
+        header( 'Location: ' . $this->openid->authUrl() );
+        exit();
+      } catch(ErrorException $e) {
+        echo "<h1>Whoops</h1>";
+        echo $e->getMessage();
+      }
+      unset( $_POST['openid_identifier'] ); // only process this once
+    }
+  }
+  
+  public function withRequired( $requiredInfo ) {
+    $this->openid->required = $requiredInfo;
+    return $this;
+  }
+  
+  public function withOptional( $optionalInfo ) {
+    $this->openid->optional = $optionalInfo;      
+    return $this;
+  }
+
+  public function getUser() {
+    $this->handleLoginRequest();
+    
+    global $_GET;
+    if( isset($_GET['openid_mode']) && $_GET['openid_mode'] != 'cancel' &&
+      $this->openid->validate() ) 
+    {
+      return array( 
+        'identity'   => $this->openid->identity,
+        'attributes' => $this->openid->getAttributes() 
+      );
+    }
+  }
+}
---
+++ demo.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Demo page for the LightOpenIDClient.php
+ */
+
+include_once 'LightOpenIDClient.php'; // this will also include LightOpenID
+
+// get an instance of the client and set some requirements
+$openid = LightOpenIDClient::getInstance()
+            ->withRequired( array('namePerson/friendly', 'contact/email') )
+            ->withOptional( array('namePerson/first') );
+
+// try to retrieve an authenticated user              
+if( $user = $openid->getUser() ) { 
+
+  print "<pre>"; 
+  print_r( $user ); 
+  print "</pre>";
+  print '<a href="demo.php">login</a>';
+
+} else { // present a login form
+
+  print <<<EOT
+    <script>
+    var openid = {
+      config : { 
+        google   : 'https://www.google.com/accounts/o8/id',
+        myopenid : 'http://myopenid.com'
+      },
+      signin : function(provider) {
+        document.getElementById('openid').value = this.config[provider];
+        document.getElementById('openidForm').submit();
+        return false;
+      }
+    };
+    </script>
+    <h3>Login using one of these OpenID Providers:</h3>
+    <a href="#" onclick="openid.signin('google');">Google</a>
+    <a href="#" onclick="openid.signin('myopenid');">MyOpenID</a>
+
+    <h3>or enter your OpenID manually...</h3>
+    <form id="openidForm" action="" method="post">
+    OpenID: <input id="openid" type="text" name="openid_identifier" /> 
+    <button>Submit</button>
+    </form>
+EOT;
+
+} 
